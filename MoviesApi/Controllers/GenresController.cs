fc845dcdfa0e4cc4509298669e9e4589e77b53bc
Ahmed Abdellatif <abdellatifahmed737@gmail.com>
2024-12-27 22:09:05 +0200

    upgrade to .NET 9.0

--- MoviesApi/Controllers/GenresController.cs
+++ MoviesApi/Controllers/GenresController.cs
@@ -1,103 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesApi.Data;
 using MoviesApi.Models;
 
-namespace MoviesApi.Controllers
+namespace MoviesApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GenresController(MoviesDbContext context) : Controller
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GenresController : Controller
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        private readonly MoviesDbContext _context;
+        return Ok(await context.Genres.ToListAsync());
+    }
 
-        public GenresController(MoviesDbContext context)
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
+        var genre = await context.Genres
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (genre == null)
         {
-              return _context.Genres != null ? 
-                          Ok(await _context.Genres.ToListAsync()) :
-                          Problem("Entity set 'MoviesDbContext.Genres'  is null.");
+            return NotFound();
         }
 
-        [HttpGet("{id:int}")]
-        public async Task<IActionResult> Details(int? id)
+        return Ok(genre);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create([Bind("Name,Description")] Genre genre)
+    {
+        context.Add(genre);
+        await context.SaveChangesAsync();
+        return Ok(genre);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Genre genre)
+    {
+        if (id != genre.Id)
         {
-            if (id == null || _context.Genres == null)
+            return NotFound();
+        }
+
+        try
+        {
+            context.Update(genre);
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!GenreExists(genre.Id))
             {
                 return NotFound();
             }
-
-            var genre = await _context.Genres
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (genre == null)
+            else
             {
-                return NotFound();
+                throw;
             }
-
-            return Ok(genre);
         }
+        return Ok(genre);
+    }
 
-
-        [HttpPost]
-        public async Task<IActionResult> Create([Bind("Name,Description")] Genre genre)
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var genre = await context.Genres.FindAsync(id);
+        if (genre != null)
         {
-            _context.Add(genre);
-            await _context.SaveChangesAsync();
-            return Ok(genre);
+            context.Genres.Remove(genre);
+            await context.SaveChangesAsync();
         }
+        return NotFound($"can't find genre with id = {id}");
+    }
 
-        [HttpPut]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Genre genre)
-        {
-            if (id != genre.Id)
-            {
-                return NotFound();
-            }
-
-            try
-            {
-                _context.Update(genre);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GenreExists(genre.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return Ok(genre);
-        }
-
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var genre = await _context.Genres.FindAsync(id);
-            if (genre != null)
-            {
-                _context.Genres.Remove(genre);
-                await _context.SaveChangesAsync();
-            }
-            return NotFound($"can't find genre with id = {id}");
-        }
-
-        private bool GenreExists(int id)
-        {
-          return (_context.Genres?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+    private bool GenreExists(int id)
+    {
+        return context.Genres.Any(e => e.Id == id);
     }
 }
--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,48 +1,18 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using MoviesApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 builder.Services.AddCors();
 
-/*builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Movies Api",
-        Description = "Movies Api Created By Ahmed",
-        TermsOfService = new Uri("https://google.com"),
-        Contact = new OpenApiContact
-        {
-            Name = "Ahmed Abdellatif",
-            Email = "ahmed@test.com",
-            Url = new Uri("https://ahmed.test")
-        }
-    });
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authrization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter your api key",
-
-    });
-});
-*/
-var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<MoviesDbContext>(options =>
 {
-    options.UseSqlServer(ConnectionString);
+    options.UseSqlServer(connectionString);
 });
 
 var app = builder.Build();
@@ -50,8 +20,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
